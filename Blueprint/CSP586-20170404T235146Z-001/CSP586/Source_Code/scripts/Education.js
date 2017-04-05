d62bfcc22bd33dd80bca25666e302e9806c804e1
Cheths <cbhaskar@hawk.iit.edu>
2017-04-04 19:03:10 -0500

    Committing drive files

---
+++ Blueprint/CSP586-20170404T235146Z-001/CSP586/Source_Code/scripts/Education.js
@@ -0,0 +1,109 @@
+let DataFrame = dfjs.DataFrame;
+let ctx = document.getElementById("myChart");
+let c = document.getElementById("newChart");
+var dataframeSet = 0;
+
+function import_data()
+{
+    var vals = document.getElementById("datasets");
+    var val = vals.options[vals.selectedIndex].value;
+    DataFrame.fromCSV(val).then(df => 
+    {
+        data = df.toJSON('SAT.json');        
+        export_data(data);
+    });
+}
+
+
+function export_data(dataset)
+{
+    var blob= new Blob([data],{ type:"text/ApplicationJson;charset:utf-8" });
+        //saveAs(blob,"sat.json");
+            debugger;
+    dataframeSet = dataset;
+    CreateTableFromJSON();
+}
+
+let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+function CreateTableFromJSON() {
+        
+        var col = [];
+        var data = JSON.parse(dataframeSet);
+        for (var i = 0; i < data.length; i++) {
+            for (var key in data[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
+
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < data.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = data[i][col[j]];
+            }
+        }
+
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer = document.getElementById("showData");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+    }
