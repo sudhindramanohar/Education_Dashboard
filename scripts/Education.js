9eaacb43bad2f62cbe981e99cd839a5f88e98c76
Cheths <cbhaskar@hawk.iit.edu>
2017-04-19 00:22:14 -0500

    Checking in the changes related to Model and plotting graphs

--- scripts/Education.js
+++ scripts/Education.js
@@ -23,52 +23,20 @@ function export_data(dataset)
         //saveAs(blob,"sat.json");
     
     dataframeSet = dataset;
+	
     CreateTableFromJSON();
 	CreateFilterElements();
+	
+	//testing chart for dummy values
+	var labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+	var data = [12, 19, 3, 5, 2, 3];
+	let pieChart = new PieChart();
+	pieChart.plot(labels,data);
+	let barChart = new BarChart();
+	barChart.plot(labels,data);
+	let lineChart = new LineChart();
+	lineChart.plot(labels,data);
 }
-
-/* let myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            xAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
- */
  
 function CreateTableFromJSON() {
 	var col = [];
@@ -81,6 +49,7 @@ function CreateTableFromJSON() {
 		}
 	}
 
+	
 	// CREATE DYNAMIC TABLE.
 	var table = document.createElement("table");
 	table.id= "dataset_table";
@@ -179,6 +148,8 @@ function CreateFilter() {
 			}	
 		}		
 		CreateAddFilterButton(select_column_div);
+		var categoricalCols = schoolProfileInfoCols.Categorical;
+		
 	}
 }
 
