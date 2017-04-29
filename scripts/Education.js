720b1364ea5f6d098b06a8dd0843659665ecd29e
Shashank Ravindranath <sravindranath@hawk.iit.edu>
2017-04-28 19:51:46 -0500

    Filter changes

--- scripts/Education.js
+++ scripts/Education.js
@@ -259,7 +259,8 @@ function createRowFilters() {
 	divContainer.innerHTML = "";
 	//show filter div
 	document.getElementById("filter-row").hidden=false;
-	var checkedCategorisedColumnsSet = getCheckedCategorisedColumns();
+	var filterData = new FilterData();
+	var checkedCategorisedColumnsSet = filterData.getCheckedCategorisedColumns();
 	
 	//for each categorical value in set build multiselect dropdown
 	for (const value of checkedCategorisedColumnsSet) {		
@@ -276,8 +277,8 @@ function createRowFilters() {
 		var selectLabel = document.createElement('label')
 		selectLabel.htmlFor = "id";
 		selectLabel.appendChild(document.createTextNode(value));
-		
-		var categoryValueSet = getAllValuesForCategory(value);
+
+		var categoryValueSet = filterData.getAllValuesForCategory(value);
 		for (const value of categoryValueSet) {
 			var option = document.createElement("option");
 			option.value = value;
@@ -292,44 +293,7 @@ function createRowFilters() {
 	}
 }
 
-/*
- * Function to get All values for particular category
- */
-function getAllValuesForCategory(categoryName) {
-	let categoryValueSet = new Set();
-	var json = getParsedJson();	
-	for(var i = 0 ; i < json.length; i++){
-		for (var key in json[i]) {
-			var columnName = key;
-			var columnValue = json[i][key];
-			if(categoryName == columnName) {
-				categoryValueSet.add(columnValue);
-			}
-		}
-	}	
-	return categoryValueSet;
-}
 
-
-/*
- * Function to get All Categorised Value based on selection of columns
- */
-function getAllCategorisedColumnSet() {
-	let categorisedColumnsSet = new Set();
-	var json = getParsedJson();
-	for(var i = 0 ; i < json.length; i++){
-		for (var key in json[i]) {
-			var columnName = key;
-			var columnValue = json[i][key];
-			if((columnName != null && columnName != '') && isNaN(columnValue)){
-				categorisedColumnsSet.add(columnName);
-			} else if(categorisedColumnsSet.size == json[i].length){
-				return categorisedColumnsSet;
-			}
-		}	
-	}		
-	return categorisedColumnsSet;
-}
 
 /*
  * Function to check if Column is Selected
@@ -366,93 +330,6 @@ function getAllColumns() {
 	return columnSet;
 }
 
-/*
- * Function to get Checked Categorised Column name
- */
-function getCheckedCategorisedColumns() {
-	let checkedCategorisedColumnSet = new Set();
-	var categorisedColumnsSet = getAllCategorisedColumnSet();
-	for (const value of categorisedColumnsSet) {		
-		if(isColumnSelected(value)){
-			checkedCategorisedColumnSet.add(value);
-		}
-	}	
-	return checkedCategorisedColumnSet;
-}
-
-/*
- * Function to get All Numerical Columns
- */
-function getAllNumericalColumns() {
-	var numericalFilterColumnsSet = new Set();
-	var allColumns = getAllColumns();
-	var categorisedColumnsSet = getAllCategorisedColumnSet();
-	for (const value of allColumns) {	
-		if(!categorisedColumnsSet.has(value)){
-			numericalFilterColumnsSet.add(value);
-		}
-	}
-	return numericalFilterColumnsSet;
-}
-
-/*
- * Function to convert string To a Camel Case
- */
-function convertToCamelCase(string,seperator){
-    var out = "";
-	if(seperator != null){
-    string.split(seperator).forEach(function (el, idx) {
-        var add = el.toLowerCase();
-        out += (idx === 0 ? add : add[0].toUpperCase() + add.slice(1));
-    });
-	}else {
-		return string;
-	}	
-    return out;
-}
-
-/*
- * Function to get all Filtered Conditions
- */
-function getAllFilteredConditions() {
-	let filteredItem = [];
-	let selectedCatColumnValueMap = new Map();
-	let selectedNumColumnValueMap = new Map();
-	let numericalCheckBox = new Set();
-	
-	//logic for categorical filter map
-	var filter = document.getElementById('categorical-filter-checkbox');
-	for(var i = 0; i < filter.children.length ; i++){
-		var childElement = filter.children[i].firstElementChild.childNodes;
-		var columnName = convertToCamelCase(childElement[0].data," "); //label field
-		var multiSelectDropDowns = childElement[2].options; // multi select dropdown field
-		let selectedCatValueSet = new Set();
-		for(var j = 0 ; j< multiSelectDropDowns.length; j++){
-			if(multiSelectDropDowns[j].selected){
-				selectedCatValueSet.add(multiSelectDropDowns[j].value);
-			}
-		}
-		if(selectedCatValueSet.size > 0){
-			selectedCatColumnValueMap.set(columnName,selectedCatValueSet);
-		}	
-	}
-	filteredItem.push(selectedCatColumnValueMap); //push categorical value map to first element in array
-	
-	//logic for numerical Values
-	var numericalFilters = getAllNumericalColumns();
-	for (const value of numericalFilters) {
-		if(isColumnSelected(value)){
-			numericalCheckBox.add(convertToCamelCase(value," "));
-		}
-	}
-	if(numericalCheckBox.size > 0){
-		selectedNumColumnValueMap.set('selectedNumericalValues',numericalCheckBox);
-		selectedNumColumnValueMap.set('numericalFilterCondition',document.querySelector('input[name="numericalFilter"]:checked').value);
-		selectedNumColumnValueMap.set('numericalFilterValue',document.getElementById('numericalFilter').value);
-	}
-	filteredItem.push(selectedNumColumnValueMap); //push numerical related values to second element in array;
-	return filteredItem;
-}
 
 /*
  * Function to reset the canvas element.
@@ -494,8 +371,9 @@ function createCanvasElement(chartType){
  */ 
 function applyChart() {
 	cleanUpSpace();
-	var filteredConditiontionsMap = getAllFilteredConditions();
-	var map = preProcessFilter(filteredConditiontionsMap);
+	var filterData = new FilterData()
+	var filteredConditiontionsMap = filterData.getAllFilteredConditions();
+	var map = filterData.preProcessFilter(filteredConditiontionsMap);
 	var chartsSelected = getChartSelections();
 	var labels = [];
 	var vals = [];
@@ -559,98 +437,6 @@ function getChartSelections(){
 		}
 	}
 	return chartsSelected;
-}
-
-class Filter{
-	constructor(){}
-	filterRow(dataSet, columnName, columnValue, operand, isCategorical, isNumerical){
-		let result;
-		if(isCategorical){
-			result = dataSet.filter(fil => fil[columnName] === columnValue );		
-		}
-
-		if(isNumerical){
-			if(operand == '='){
-				result = dataSet.filter(fil => fil[columnName] === Number.parseInt(columnValue) );
-			}
-			else if(operand == '>='){
-				result = dataSet.filter(fil => fil[columnName] >= Number.parseInt(columnValue) );
-			}
-			else{
-				result = dataSet.filter(fil => fil[columnName] <= Number.parseInt(columnValue) );
-			}
-		}
-		return result.length;
-    }
-}
-
-function preProcessFilter(filterCondition)
-{
-	let isCategorical = 0;
-	let isNumerical = 0;
-	let columnName;
-	let categoryValues;
-	let columnValue;
-	let operand;
-	let colNames = [];
-	let resultDataMap = new Map();
-	for (let categoryKey of filterCondition[0].keys()){
-		isCategorical = 1;
-		isNumerical = 0;
-		columnName = categoryKey;
-		categoryValues = filterCondition[0].get(categoryKey);
-		for(let columnVal of categoryValues){
-			let count = filterData(columnName, columnVal, isCategorical, isNumerical);
-			resultDataMap.set(columnVal, count);
-		}
-	}
-
-	for(let numericalKey of filterCondition[1].keys())
-	{
-		isNumerical = 1;
-		isCategorical = 0;		
-		if(numericalKey == "selectedNumericalValues"){
-			let numericalVals = filterCondition[1].get(numericalKey);
-			
-			for(let val of numericalVals.keys())
-			{
-				colNames.push(val);
-			}
-		}
-		else if(numericalKey == "numericalFilterCondition")
-		{
-			let operandName = filterCondition[1].get(numericalKey);
-			if(operandName == "greaterThanEqualTo"){
-				operand = ">=";
-			}
-			else if(operandName == "lessThanEqualTo"){
-				operand = "<=";
-			}
-			else if(operandName == "equalTo"){
-				operand = "=";
-			}
-		}
-		else if(numericalKey == "numericalFilterValue"){
-			columnValue = filterCondition[1].get(numericalKey);
-		}
-		
-	}
-	
-	if(isNumerical){
-		for(let col of colNames)
-		{
-			let count = filterData(col,columnValue, isCategorical, isNumerical, operand);
-			resultDataMap.set(col, count);
-		}
-	}
-	return resultDataMap;
-}
-
-function filterData(columnName,columnValue,isCategorical, isNumerical, operand = '=')
-{
-	let filterdata = new Filter();
-	let result = filterdata.filterRow(objArr, columnName, columnValue, operand, isCategorical, isNumerical);
-	return result;
 }
 
 
